2455193a46ffe0abe094faa2586aaf9e31c81f2d
Himanshu001-U <himanshuupadhayay2021@gmail.com>
2024-07-11 21:59:06 +0530

    Initial Commit

---
+++ src/components/body.jsx
@@ -0,0 +1,28 @@
+import "./body.css"
+function Body() {
+    return(
+        <div className="body">
+            <div className="box">
+                <img src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiYaYOYBas0hKnzvFclz0v1JaPU7w0YzxJNKXPxmGYS0W1nMpGvQgRWQBrRSpuojOgbWJqy9447EguyiOnKMoFmXz7wgmC2dZZiMlz3wH1DP09epPsrobuJ9AkP68o2WSNxpM1CVKOHwjg/s1600/Make+a+Movie+Poster+With+Texture+Background+In+Photoshop.jpg" 
+                alt="poster" />
+                <h1>The Legend</h1>
+                <p className="info">2017, David Ayer</p>
+                <p className="title">118 min Action/Thriller/Comedy</p>
+                <p className="para">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil delectus explicabo illum earum. <br />Consequuntur voluptas incidunt quis ducimus, quisquam sit aperiam 
+                    Lorem ipsum dolor, sit amet <br /> consectetur adipisicinone eum natus sed unde quod. Officiis, architecto nihil.ut at adipisci, alias nisi.</p>
+
+            </div>
+            <div className="box">
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS-qoqxLVze0wzLtxeiQlZ9iKJXm_-2ndrcjWzj_wriL5UtDS6zfHmlfMrNoWSK1S8quyU&usqp=CAU" 
+                alt="poster" />
+                <h1 className="head">Beast</h1>
+                <p className="information">2017, David Ayer</p>
+                <p className="tit">118 min Action/Thriller/Comedy</p>
+                <p className="paragraph">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil delectus <br />explicabo illum earum nemo similique repellendus. Consequuntur voquis ducimus, quisquam sit aperiam <br />
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ut dolore ab tci, alias nisi.</p>
+            </div>
+        </div>
+    )
+}
+
+export default Body
